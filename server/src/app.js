8f81a1d4b15dd6bd2b1f8a8c4f7caccecb886d1c
Kevin <devadderkevin@gmail.com>
2018-11-18 03:18:17 +0200

    Added some packages

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,10 @@
-console.log('working now')
+const express = require('express');
+
+//set up express app
+const app = express();
+
+
+//start app
+app.listen(process.env.port || 5000, function(){
+	console.log('Running on port 5000')
+})
